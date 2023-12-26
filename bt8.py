6aa5bac41b64cc99f181f88e58680b3acfe251e5
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-12-26 20:01:39 +0700

    update trie method

--- bt8.py
+++ bt8.py
@@ -1,11 +1,16 @@
+# List products 
 products = ["cat", "banana", "obama", "car", "cow", "alibaba", "batman"]
 
+# Get product name input
 product_name = input("Enter product name: ")
 
+# List store the result indexes contains the product name
 product_indexes = []
 
+# Iterate through the product list
 for product in products: 
-    if product.__contains__(product_name):
+    # Check if the product string contains the product name (substring)
+    if product_name in product:
         product_indexes.append(products.index(product))
 
 print("Vị trí các index chứa tên '{}': ".format(product_name), product_indexes)
